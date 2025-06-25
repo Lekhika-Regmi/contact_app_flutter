5d08483f097cf12a8d9762f60026d4c1fac6bb7c
Lekhika <rlekhika.regmi@gmail.com>
2025-06-25 13:26:26 +0545

    feat: implement contact repository with local and remote data handling

---
+++ lib/features/contacts/data/repositories/contact_repository_impl.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'contact_repository_impl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$contactRepositoryHash() => r'dd813238deda9b130fc4bece396112d1716fb1c9';
+
+/// See also [contactRepository].
+@ProviderFor(contactRepository)
+final contactRepositoryProvider =
+    AutoDisposeProvider<ContactRepository>.internal(
+      contactRepository,
+      name: r'contactRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$contactRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ContactRepositoryRef = AutoDisposeProviderRef<ContactRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
