e1637709a26eb9d128e8e989f6b772fa32e72c72
Lekhika <rlekhika.regmi@gmail.com>
2025-06-25 13:23:44 +0545

    feat: implement local and remote data sources for contacts

---
+++ lib/features/contacts/data/datasources/contact_local_datasource.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'contact_local_datasource.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$contactLocalDataSourceHash() =>
+    r'9f85709479e5f69afaebf7ecbb033db03ee35784';
+
+/// See also [contactLocalDataSource].
+@ProviderFor(contactLocalDataSource)
+final contactLocalDataSourceProvider =
+    AutoDisposeProvider<ContactLocalDataSource>.internal(
+      contactLocalDataSource,
+      name: r'contactLocalDataSourceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$contactLocalDataSourceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ContactLocalDataSourceRef =
+    AutoDisposeProviderRef<ContactLocalDataSource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
---
+++ lib/features/contacts/data/datasources/contact_remote_datasource.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'contact_remote_datasource.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$contactRemoteDataSourceHash() =>
+    r'5aeb3c0286e156d92002ea85bdb6c4e3d3f12a69';
+
+/// See also [contactRemoteDataSource].
+@ProviderFor(contactRemoteDataSource)
+final contactRemoteDataSourceProvider =
+    AutoDisposeProvider<ContactRemoteDataSource>.internal(
+      contactRemoteDataSource,
+      name: r'contactRemoteDataSourceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$contactRemoteDataSourceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ContactRemoteDataSourceRef =
+    AutoDisposeProviderRef<ContactRemoteDataSource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
