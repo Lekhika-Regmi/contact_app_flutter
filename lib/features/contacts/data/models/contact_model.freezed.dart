638365f16eee1b69172ebfd3ed97a716a6865f72
Lekhika <rlekhika.regmi@gmail.com>
2025-06-25 12:56:19 +0545

    feat: create contact entity and model with freezed

---
+++ lib/features/contacts/data/models/contact_model.freezed.dart
@@ -0,0 +1,316 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'contact_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
+);
+
+ContactModel _$ContactModelFromJson(Map<String, dynamic> json) {
+  return _ContactModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$ContactModel {
+  int? get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get phone => throw _privateConstructorUsedError;
+  String? get email => throw _privateConstructorUsedError;
+  String? get avatar => throw _privateConstructorUsedError;
+  @JsonKey(name: 'created_at')
+  String? get createdAt => throw _privateConstructorUsedError;
+  @JsonKey(name: 'updated_at')
+  String? get updatedAt => throw _privateConstructorUsedError;
+
+  /// Serializes this ContactModel to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of ContactModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $ContactModelCopyWith<ContactModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ContactModelCopyWith<$Res> {
+  factory $ContactModelCopyWith(
+    ContactModel value,
+    $Res Function(ContactModel) then,
+  ) = _$ContactModelCopyWithImpl<$Res, ContactModel>;
+  @useResult
+  $Res call({
+    int? id,
+    String name,
+    String phone,
+    String? email,
+    String? avatar,
+    @JsonKey(name: 'created_at') String? createdAt,
+    @JsonKey(name: 'updated_at') String? updatedAt,
+  });
+}
+
+/// @nodoc
+class _$ContactModelCopyWithImpl<$Res, $Val extends ContactModel>
+    implements $ContactModelCopyWith<$Res> {
+  _$ContactModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of ContactModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = null,
+    Object? phone = null,
+    Object? email = freezed,
+    Object? avatar = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+  }) {
+    return _then(
+      _value.copyWith(
+            id: freezed == id
+                ? _value.id
+                : id // ignore: cast_nullable_to_non_nullable
+                      as int?,
+            name: null == name
+                ? _value.name
+                : name // ignore: cast_nullable_to_non_nullable
+                      as String,
+            phone: null == phone
+                ? _value.phone
+                : phone // ignore: cast_nullable_to_non_nullable
+                      as String,
+            email: freezed == email
+                ? _value.email
+                : email // ignore: cast_nullable_to_non_nullable
+                      as String?,
+            avatar: freezed == avatar
+                ? _value.avatar
+                : avatar // ignore: cast_nullable_to_non_nullable
+                      as String?,
+            createdAt: freezed == createdAt
+                ? _value.createdAt
+                : createdAt // ignore: cast_nullable_to_non_nullable
+                      as String?,
+            updatedAt: freezed == updatedAt
+                ? _value.updatedAt
+                : updatedAt // ignore: cast_nullable_to_non_nullable
+                      as String?,
+          )
+          as $Val,
+    );
+  }
+}
+
+/// @nodoc
+abstract class _$$ContactModelImplCopyWith<$Res>
+    implements $ContactModelCopyWith<$Res> {
+  factory _$$ContactModelImplCopyWith(
+    _$ContactModelImpl value,
+    $Res Function(_$ContactModelImpl) then,
+  ) = __$$ContactModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({
+    int? id,
+    String name,
+    String phone,
+    String? email,
+    String? avatar,
+    @JsonKey(name: 'created_at') String? createdAt,
+    @JsonKey(name: 'updated_at') String? updatedAt,
+  });
+}
+
+/// @nodoc
+class __$$ContactModelImplCopyWithImpl<$Res>
+    extends _$ContactModelCopyWithImpl<$Res, _$ContactModelImpl>
+    implements _$$ContactModelImplCopyWith<$Res> {
+  __$$ContactModelImplCopyWithImpl(
+    _$ContactModelImpl _value,
+    $Res Function(_$ContactModelImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of ContactModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = null,
+    Object? phone = null,
+    Object? email = freezed,
+    Object? avatar = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+  }) {
+    return _then(
+      _$ContactModelImpl(
+        id: freezed == id
+            ? _value.id
+            : id // ignore: cast_nullable_to_non_nullable
+                  as int?,
+        name: null == name
+            ? _value.name
+            : name // ignore: cast_nullable_to_non_nullable
+                  as String,
+        phone: null == phone
+            ? _value.phone
+            : phone // ignore: cast_nullable_to_non_nullable
+                  as String,
+        email: freezed == email
+            ? _value.email
+            : email // ignore: cast_nullable_to_non_nullable
+                  as String?,
+        avatar: freezed == avatar
+            ? _value.avatar
+            : avatar // ignore: cast_nullable_to_non_nullable
+                  as String?,
+        createdAt: freezed == createdAt
+            ? _value.createdAt
+            : createdAt // ignore: cast_nullable_to_non_nullable
+                  as String?,
+        updatedAt: freezed == updatedAt
+            ? _value.updatedAt
+            : updatedAt // ignore: cast_nullable_to_non_nullable
+                  as String?,
+      ),
+    );
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ContactModelImpl implements _ContactModel {
+  const _$ContactModelImpl({
+    this.id,
+    required this.name,
+    required this.phone,
+    this.email,
+    this.avatar,
+    @JsonKey(name: 'created_at') this.createdAt,
+    @JsonKey(name: 'updated_at') this.updatedAt,
+  });
+
+  factory _$ContactModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ContactModelImplFromJson(json);
+
+  @override
+  final int? id;
+  @override
+  final String name;
+  @override
+  final String phone;
+  @override
+  final String? email;
+  @override
+  final String? avatar;
+  @override
+  @JsonKey(name: 'created_at')
+  final String? createdAt;
+  @override
+  @JsonKey(name: 'updated_at')
+  final String? updatedAt;
+
+  @override
+  String toString() {
+    return 'ContactModel(id: $id, name: $name, phone: $phone, email: $email, avatar: $avatar, createdAt: $createdAt, updatedAt: $updatedAt)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ContactModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.phone, phone) || other.phone == phone) &&
+            (identical(other.email, email) || other.email == email) &&
+            (identical(other.avatar, avatar) || other.avatar == avatar) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.updatedAt, updatedAt) ||
+                other.updatedAt == updatedAt));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(
+    runtimeType,
+    id,
+    name,
+    phone,
+    email,
+    avatar,
+    createdAt,
+    updatedAt,
+  );
+
+  /// Create a copy of ContactModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ContactModelImplCopyWith<_$ContactModelImpl> get copyWith =>
+      __$$ContactModelImplCopyWithImpl<_$ContactModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ContactModelImplToJson(this);
+  }
+}
+
+abstract class _ContactModel implements ContactModel {
+  const factory _ContactModel({
+    final int? id,
+    required final String name,
+    required final String phone,
+    final String? email,
+    final String? avatar,
+    @JsonKey(name: 'created_at') final String? createdAt,
+    @JsonKey(name: 'updated_at') final String? updatedAt,
+  }) = _$ContactModelImpl;
+
+  factory _ContactModel.fromJson(Map<String, dynamic> json) =
+      _$ContactModelImpl.fromJson;
+
+  @override
+  int? get id;
+  @override
+  String get name;
+  @override
+  String get phone;
+  @override
+  String? get email;
+  @override
+  String? get avatar;
+  @override
+  @JsonKey(name: 'created_at')
+  String? get createdAt;
+  @override
+  @JsonKey(name: 'updated_at')
+  String? get updatedAt;
+
+  /// Create a copy of ContactModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$ContactModelImplCopyWith<_$ContactModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/features/contacts/data/models/contact_model.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'contact_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ContactModelImpl _$$ContactModelImplFromJson(Map<String, dynamic> json) =>
+    _$ContactModelImpl(
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String,
+      phone: json['phone'] as String,
+      email: json['email'] as String?,
+      avatar: json['avatar'] as String?,
+      createdAt: json['created_at'] as String?,
+      updatedAt: json['updated_at'] as String?,
+    );
+
+Map<String, dynamic> _$$ContactModelImplToJson(_$ContactModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'phone': instance.phone,
+      'email': instance.email,
+      'avatar': instance.avatar,
+      'created_at': instance.createdAt,
+      'updated_at': instance.updatedAt,
+    };
